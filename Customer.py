14d44c7083bb71d2dfd655f8517423120a993b9c
YordanSV <yordan10_pv@hotmail.com>
2023-10-26 21:02:18 -0600

    Clases iniciales

---
+++ Customer.py
@@ -0,0 +1,11 @@
+
+
+
+class Customer:
+
+    def __init__(self, id, name, phone, email):
+        self.id = id
+        self.nombre = name
+        self.phone = phone
+        self.email = email
+    
---
+++ ParkingSpace.py
@@ -0,0 +1,8 @@
+
+class ParkingSpace:
+    def __init__(self, id, spaceType, status):
+        self.id = id
+        self.spaceType = spaceType
+        self.status = status
+
+
